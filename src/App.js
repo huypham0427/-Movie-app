1c8d19c5b4847dfda28b8b871a92cd2459efa677
huypham0427 <59068418+huypham0427@users.noreply.github.com>
2022-05-31 12:56:13 -0700

    reduce-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
+import MovieBox from './MovieBox';
+import { Button, Navbar, Container, Nav, FormControl, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as ReactBootStrap from 'react-bootstrap';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import { Navigate } from 'react-router-dom';
+import Home from './component/Home';
+
+const API_URL="https://api.themoviedb.org/3/movie/now_playing?api_key=151f5123399aa60296034f5094c257e3&language=en-US&page=1&fbclid=IwAR07o-_oLvsPWeAJQ0VwYxI2kEAaTA2UPo2XNrBqucDNfCL5Au0RBXSF8nk";
+
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  //Create a query for search bar
+  const [query, setQuery] = useState('');
+  //Spinner loading API
+  const [loading, setLoading] = useState(false);
+  //Fetch API
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then(data =>{
+      console.log(data)
+      setMovies(data.results);
+    })
+  }, [])
+
+  // Asynchronous call for Search function
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try{
+      const url =`https://api.themoviedb.org/3/search/movie?api_key=151f5123399aa60296034f5094c257e3&query=${query}`;
+      const res = await fetch(url);
+        if(!res.ok){  
+          throw Error('Could not fetch the data');
+        }
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results); 
+      setLoading(true);
+    }
+    catch(e){
+      console.log(e.message);
+    }
+  }
+
+  const changeHandler=(e)=>{
+    setQuery(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">Movee</Navbar.Brand>
+          <Navbar.Brand href="">Trending</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+            <Navbar.Collapse id="nabarScroll">
+              <Nav className="me-auto my-2 my-lg-3"
+              style={{maxHeight:'100px'}}navbarScroll></Nav>
+
+              {/* Search bar */}
+              <Form className="d-flex" onSubmit={searchMovie}>
+                <FormControl 
+                type="search"
+                placeholder="Search movies"
+                className="me-2"
+                aria-label="search"
+                name=""
+                value={query} onChange={changeHandler} ></FormControl>
+                <Button variant="secondary" type="submit">Search
+                <ReactBootStrap.Spinner animation="border" variant='primary' />
+                </Button>
+              </Form>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        {movies.length > 0 ?(
+        <div className="container">
+          <div className="grid">
+            {movies.map((movieReq)=>
+            <MovieBox key={movieReq.id} {...movieReq}/>)}
+          </div>
+        </div>
+        ):(
+          <h2>Sorry !! No Movies Found</h2>
+        )}
+        {/* <ReactBootStrap.Spinner animation="border" variant='primary' /> */}
+      </div>   
+    </>
+
   );
 }
+
 
 export default App;
